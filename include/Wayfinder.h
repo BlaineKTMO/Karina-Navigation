810445c47669cc3e3f456edf1fc4d9016bd5835e
BlaineKTMO <Blaine.Oania@gmail.com>
2023-05-04 16:50:10 -0400

    Minor changes to finish integration

--- include/Wayfinder.h
+++ include/Wayfinder.h
@@ -116,7 +116,7 @@ public:
         
         // Set initial status values
         reverseStatus(false),
-        vfhStatus(false),
+        vfhStatus(true),
         lfStatus(false),
         stopStatus(false),
 
